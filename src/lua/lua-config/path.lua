eebd25b1728a159a29b91727e8bed71b27b90110
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-11 12:44:32 +0100

    sync

--- src/lua/lua-config/path.lua
+++ src/lua/lua-config/path.lua
@@ -4,19 +4,19 @@ local lfs = lfs
 local path = {}
 
 --- Resolves path and addes hostname if directory is found 
----@param path string
+---@param path_str string
 ---@return string
-function path.resolve_path(path)
-    if path:sub(path:len(), path:len()) ~= "/" then
-        path = path .. "/"
+function path.add_hostname_if_found(path_str)
+    if path_str:sub(path_str:len(), path_str:len()) ~= "/" then
+        path_str = path_str .. "/"
     end
 
-    local hostname_path = path .. config.env.hostname
+    local hostname_path = path_str .. config.env.hostname
     if lfs.exists(hostname_path) and lfs.attributes(hostname_path).mode == "directory" then
         return hostname_path
     end
 
-    return path
+    return path_str
 end
 
 ---@param path string
