bc1499cf717eaeeaaded0b07387bfa96d1ab9029
Freemaker <ralf.rose.oskar@gmail.com>
2025-10-08 16:47:41 +0200

    fix loading lua-config

--- src/lua/lua-config/lib/lua-config.lua
+++ src/lua/lua-config/lib/lua-config.lua
@@ -1,3 +1,6 @@
 local lib = package.loadlib(__lua_config_dir .. "/zig-out/bin/lua_config.dll", "luaopen_lua_config")
+if not lib then
+    error("unable to load lua-config library")
+end
 
-return lib
+return lib()
