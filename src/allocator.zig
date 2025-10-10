18e7c7cc6798bec09af840900c60c506c4278f07
Freemaker <ralf.rose.oskar@gmail.com>
2025-10-10 20:04:59 +0200

    just free everything if allocator gets collected

--- src/allocator.zig
+++ src/allocator.zig
@@ -12,21 +12,31 @@ pub const __luaMeta = Lua.StructMeta{
     },
 };
 
-gpa: std.heap.GeneralPurposeAllocator(if (builtin.mode == .Debug) .{
+var gpa: std.heap.GeneralPurposeAllocator(if (builtin.mode == .Debug) .{
     .retain_metadata = true,
     .never_unmap = true,
-} else .{}),
+} else .{}) = .init;
+
+arena: if (builtin.mode == .Debug) void else std.heap.ArenaAllocator,
 
 pub fn init() Allocator {
     return Allocator{
-        .gpa = .init,
+        .arena = if (builtin.mode == .Debug) {} else std.heap.ArenaAllocator.init(gpa.allocator()),
     };
 }
 
 pub fn deinit(self: *Allocator) void {
-    _ = self.gpa.deinit();
+    if (comptime builtin.mode != .Debug) {
+        self.arena.deinit();
+    }
+
+    _ = gpa.deinit();
 }
 
 pub fn allocator(self: *Allocator) std.mem.Allocator {
-    return self.gpa.allocator();
+    if (comptime builtin.mode == .Debug) {
+        return gpa.allocator();
+    }
+
+    return self.arena.allocator();
 }
