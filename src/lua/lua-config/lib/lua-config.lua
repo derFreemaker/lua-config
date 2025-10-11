e4ea1a3c0fb57fbf1a42c1064f5191376928341c
Freemaker <ralf.rose.oskar@gmail.com>
2025-10-11 11:37:43 +0200

    more checking for loading lua-config lib

--- src/lua/lua-config/lib/lua-config.lua
+++ src/lua/lua-config/lib/lua-config.lua
@@ -7,15 +7,20 @@ local function get_os()
     end
 end
 
-local lib, msg
+local lib_func, msg
 if get_os() == "windows" then
-    lib, msg = package.loadlib(__lua_config_dir .. "/zig-out/bin/lua_config.dll", "luaopen_lua_config")
+    lib_func, msg = package.loadlib(__lua_config_dir .. "/zig-out/bin/lua_config.dll", "luaopen_lua_config")
 else
-    lib, msg = package.loadlib(__lua_config_dir .. "/zig-out/lib/liblua_config.so", "luaopen_lua_config")
+    lib_func, msg = package.loadlib(__lua_config_dir .. "/zig-out/lib/liblua_config.so", "luaopen_lua_config")
 end
 
-if not lib then
+if not lib_func then
     error("unable to load lua-config library: " .. msg)
 end
 
-return lib()
+local success, lib_obj = pcall(lib_func)
+if not success then
+    error("unable to create lua-config object: " .. lib_obj)
+end
+
+return lua_obj
